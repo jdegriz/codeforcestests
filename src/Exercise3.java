d69c58049070ba5c7c473c5aecf71ed0eb45733b
Andrei Kakhanov <kakhanov.andrey@gmail.com>
2018-02-07 19:30:26 +0300

    Initial commit

---
+++ src/Exercise3.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Exercise3 extends Thread {
+
+    static BufferedReader in;
+    static PrintWriter out;
+    static StringTokenizer st;
+
+    private void solve() throws IOException {
+    }
+
+    public static void main(String[] args) {
+        new Exercise3().run();
+    }
+
+    @Override
+    public void run() {
+        try {
+            in = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(System.out);
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+
+    private String nextToken() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            String line = in.readLine();
+
+            if (line == null) {
+                return null;
+            }
+
+            st = new StringTokenizer(line);
+        }
+
+        return st.nextToken();
+    }
+
+    private int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    private long nextLong() throws IOException {
+        return Long.parseLong(nextToken());
+    }
+
+    private double nextDouble() throws IOException {
+        return Double.parseDouble(nextToken());
+    }
+}
